69cbd3c0fa790a37d0dc6dd651ec7f7825f3b383
Niesa <niesamaghfiroh27@gmail.com>
2026-03-22 09:27:13 +0700

    LS 5 Done

--- app/(dashboard)/components/categories/category-table.tsx
+++ app/(dashboard)/components/categories/category-table.tsx
@@ -1,24 +1,19 @@
+import { getImageUrl } from "@/app/lib/api";
+import { Category } from "@/app/types";
 import priceFormatter from "@/app/utils/price-formatter";
 import Image from "next/image";
 import { FiEdit2, FiTrash2 } from "react-icons/fi";
 
-const categoryData = [
-    {
-    name: "Running",
-    imageUrl: "/images/categories/category-running.png",
-    description: "lorem ipsum", 
-    },
-    {
-    name: "Football",
-    imageUrl: "/images/categories/category-football.png",
-    description: "lorem ipsum", 
-    },
-];
+type TCategoryTableProps = {
+  categories: Category[];
+  onEdit: (category: Category) => void;
+  onDelete: (id: string) => void;
+}
 
-const CategoryTable = () => {
-    return (
-        <div className="bg-white rounded-xl border border-gray-200">
-            <table className="w-full text-left border-collapse">
+const CategoryTable = ({categories, onEdit, onDelete}: TCategoryTableProps) => {
+  return (
+    <div className="bg-white rounded-xl border border-gray-200">
+      <table className="w-full text-left border-collapse">
         <thead>
           <tr className="border-b border-gray-200">
             <th className="px-6 py-4 font-semibold">Category Name</th>
@@ -27,39 +22,46 @@ const CategoryTable = () => {
           </tr>
         </thead>
         <tbody>
-            {
-                categoryData.map ((data, index) => (
-                    <tr key={index} className="border-b border-gray-200 last:border-b-0">
-                        <td className="px-6 py-4 font-medium">
-                            <div className="flex gap-2 items-center">
-                                <div className="aspect-square bg-gray-100 rounded-md">
-                                    <Image
-                                    src={data.imageUrl} 
-                                    width={52} 
-                                    height={52}
-                                    alt={data.name}
-                                    className="aspect-square object-contain"
-                                    />
-                                </div>
-                                <span>{data.name}</span>
-                            </div>
-                        </td>
-                        <td className="px-6 py-4 font-medium">{data.description}</td>
-                        <td className="px-6 py-7.5 flex items-center gap-3 text-gray-600">
-                            <button>
-                                <FiEdit2 size={20}/>
-                            </button>
-                             <button>
-                                <FiTrash2 size={20}/>
-                            </button>
-                        </td>
-                    </tr>
-                )) 
-            }
+          {categories.map((data, index) => (
+            <tr
+              key={index}
+              className="border-b border-gray-200 last:border-b-0"
+            >
+              <td className="px-6 py-4 font-medium">
+                <div className="flex gap-2 items-center">
+                  <div className="aspect-square bg-gray-100 rounded-md">
+                    <Image
+                      src={getImageUrl(data.imageUrl)}
+                      width={52}
+                      height={52}
+                      alt={data.name}
+                      className="aspect-square object-contain"
+                    />
+                  </div>
+                  <span>{data.name}</span>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-medium">{data.description}</td>
+              <td className="px-6 py-7.5 flex items-center gap-3 text-gray-600">
+                <button
+                  onClick={() => onEdit?.(data)}
+                  className="cursor-pointer"
+                >
+                  <FiEdit2 size={20} />
+                </button>
+                <button
+                  onClick={() => onDelete?.(data._id)}
+                  className="cursor-pointer"
+                >
+                  <FiTrash2 size={20} />
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-        </table>
-        </div>
-    )
+      </table>
+    </div>
+  )
 }
 
 export default CategoryTable;
--- app/(dashboard)/components/layouts/sidebar.tsx
+++ app/(dashboard)/components/layouts/sidebar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import Button from "@/app/(landing)/components/ui/button";
+import { logout } from "@/app/services/auth.services";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { FiBox, FiCreditCard, FiLayers, FiLogOut, FiShoppingCart } from "react-icons/fi";
 
 const Sidebar = () => {
     const pathname= usePathname ();
+    const {push} = useRouter()
 
     const menuItems = [
     {
@@ -30,6 +33,11 @@ const Sidebar = () => {
       link: "/admin/bank-info",
     },
   ];
+
+  const handleLogout = () => {
+    logout();
+    push("/admin/login");
+  }
 
 
     return <aside className="w-80 min-h-screen bg-white border-r border-gray-100 flex flex-col fixed left-0 top-0">
@@ -58,13 +66,13 @@ const Sidebar = () => {
           );
         })}
       </div>
-      <Link
-        href="#"
-        className="flex gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10"
+      <Button
+        className="flex cursor-pointer gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10"
+        onClick={handleLogout}
       >
         <FiLogOut size={24} />
         Log Out
-      </Link>
+      </Button>
     </aside>;
 }; 
 
